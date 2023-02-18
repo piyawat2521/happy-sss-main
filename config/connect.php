8a07459d0a6e75086fee31034ae18f044b6a76dc
piyawat2521 <115604287+piyawat2521@users.noreply.github.com>
2023-02-18 12:25:16 +0700

    new1

--- config/connect.php
+++ config/connect.php
@@ -8,5 +8,3 @@ $db = "db_wellness_happy";
 $coon = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
 $coon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $coon->exec("set names utf8");
-
-echo "insert";
--- insert.php
+++ insert.php
@@ -2,6 +2,15 @@
 session_start();
 include 'config/connect.php';
 
-$_POST["sex"] . "-" . $_POST["age"] . "-" . $_POST["q3"] . "-" . $_POST["q3t"] . "-" . $_POST["q4"] . "-" . $_POST["q4_1"] . "-" . $_POST["q4_1t"] . "-" . $_POST["q4_2"] . "-" . $_POST["q4_2t"] . "-" . $_POST["weight"] . "-" . $_POST["height"] . "-" . $_POST["waist"];
+$dateTimer = date("d/m/") . (date("Y") + 543) . date("-h:i:s");
+$sess = 11111;
 
-$stmt = $coon->prepare(" INSERT INTO `data_all`(`data_id`, `sex`, `age`, `q3`, `q3t`, `q4`, `q4_1`, `q4_1t`, `q4_2`, `q4_2t`, `weight`, `height`, `waist`, `q6`, `q7`, `q8`, `q9`, `q10`, `q11`, `q12`, `q13`, `q14`, `q15`, `q16`, `q17`, `q18`, `q19`, `q20`, `q21`, `date_time`, `code`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8],[value-9],[value-10],[value-11],[value-12],[value-13],[value-14],[value-15],[value-16],[value-17],[value-18],[value-19],[value-20],[value-21],[value-22],[value-23],[value-24],[value-25],[value-26],[value-27],[value-28],[value-29],[value-30],[value-31]  ");
+
+$stmt = $coon->prepare(" INSERT INTO data_all(data_id, sex, age, q3, q3t, q4, q4_1, q4_1t, q4_2, q4_2t, weights, height, waist, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16, q17, q18, q19, q20, q21, date_time, code) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+$stmt->execute([$_POST["sex"], $_POST["age"], $_POST["q3"], $_POST["q3t"], $_POST["q4"], $_POST["q4_1"], $_POST["q4_1t"], $_POST["q4_2"], $_POST["q4_2t"], $_POST["weight"], $_POST["height"], $_POST["waist"], $_POST["q6"], $_POST["q7"], $_POST["q8"], $_POST["q9"], $_POST["q10"], $_POST["q11"], $_POST["q12"], $_POST["q13"], $_POST["q14"], $_POST["q15"], $_POST["q16"], $_POST["q17"], $_POST["q18"], $_POST["q19"], $_POST["q20"], $_POST["q21"], $dateTimer, $sess]);
+
+// if (empty($stmt)) {
+//     echo "No Insert";
+// } else {
+//     echo "Ok";
+// }
